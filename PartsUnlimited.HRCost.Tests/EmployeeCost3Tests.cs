7016a57a86c61de616f3279cfd9dc516bae233bb
Thomas Thiry <thomas.thiry@gmail.com>
2024-09-17 22:48:26 +0200

    Add EmployeeCost3Tests

---
+++ PartsUnlimited.HRCost.Tests/EmployeeCost3Tests.cs
@@ -0,0 +1,31 @@
+using NFluent;
+using PartsUnlimited.HRCost.Web.ViewModels;
+using static PartsUnlimited.HRCost.Tests.AppBuilder;
+using static PartsUnlimited.HRCost.Tests.EmployeeBuilder;
+
+namespace PartsUnlimited.HRCost.Tests;
+
+public class EmployeeCost3Tests
+{
+    [Fact]
+    public void The_details_of_an_employee_can_be_viewed()
+    {
+        var employee = AnEmployee().Build();
+        var context = AnApp().With(employee).Build();
+        
+        var retrievedEmployee = context.EmployeeController.Edit(employee.Id).To<EmployeeViewModel>();
+
+        Check.That(retrievedEmployee.LastName).Is(employee.LastName);
+    }
+    
+    [Fact]
+    public void The_yearly_gross_salary_of_an_employee_is_12_times_the_monthly_gross_salary()
+    {
+        var employee = AnEmployee().WithMonthlyGrossSalary(3000m).Build();
+        var app = AnApp().With(employee).Build();
+
+        var retrievedEmployee = app.EmployeeController.Edit(employee.Id).To<EmployeeViewModel>();
+
+        Check.That(retrievedEmployee.YearlyGrossSalaryCost).Is(36000); // 12 * 3000
+    }
+}
