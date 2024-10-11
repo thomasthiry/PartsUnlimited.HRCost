8c2f398cfc06570fbed337fbd4edfa3cb919c31f
Thomas Thiry <thomas.thiry@gmail.com>
2024-10-11 17:23:11 +0200

    Move Database to a file

---
+++ PartsUnlimited.HRCost.Tests/Database.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+namespace PartsUnlimited.HRCost.Tests;
+
+public class Database
+{
+    private readonly string _databaseName;
+    public string ConnectionString { get; }
+
+    public Database(string connectionString, string databaseName)
+    {
+        var timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+        
+        _databaseName = $"{databaseName}_{timestamp}";
+        ConnectionString = connectionString;
+    }
+
+    public Task Create()
+    {
+        return ExecuteWithoutUsing($@"CREATE DATABASE {_databaseName};");
+    }
+
+    public async Task Execute(string script)
+    {
+        var scriptUsingDb = $"USE {_databaseName};\n {script}";
+        await ExecuteWithoutUsing(scriptUsingDb);
+    }
+    
+    private async Task ExecuteWithoutUsing(string script)
+    {
+        await using var connection = new SqlConnection(ConnectionString);
+        
+        await connection.OpenAsync();
+
+        var numberOfRowasAffected = await connection.ExecuteAsync(script);
+    }
+
+    public async Task Drop()
+    {
+        await ExecuteWithoutUsing($"ALTER DATABASE [{_databaseName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE");
+        await ExecuteWithoutUsing($"DROP DATABASE [{_databaseName}]");
+    }
+}
--- PartsUnlimited.HRCost.Tests/DatabaseFixture.cs
+++ PartsUnlimited.HRCost.Tests/DatabaseFixture.cs
@@ -1,6 +1,4 @@
-﻿using System.Data.SqlClient;
-using System.Diagnostics;
-using Dapper;
+﻿using System.Diagnostics;
 
 namespace PartsUnlimited.HRCost.Tests;
 
@@ -21,45 +19,5 @@ public class DatabaseFixture : IDisposable
     public void Dispose()
     {
         _database.Drop().GetAwaiter().GetResult();
-    }
-}
-
-public class Database
-{
-    private readonly string _databaseName;
-    public string ConnectionString { get; }
-
-    public Database(string connectionString, string databaseName)
-    {
-        var timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
-        
-        _databaseName = $"{databaseName}_{timestamp}";
-        ConnectionString = connectionString;
-    }
-
-    public Task Create()
-    {
-        return ExecuteWithoutUsing($@"CREATE DATABASE {_databaseName};");
-    }
-
-    public async Task Execute(string script)
-    {
-        var scriptUsingDb = $"USE {_databaseName};\n {script}";
-        await ExecuteWithoutUsing(scriptUsingDb);
-    }
-    
-    private async Task ExecuteWithoutUsing(string script)
-    {
-        await using var connection = new SqlConnection(ConnectionString);
-        
-        await connection.OpenAsync();
-
-        var numberOfRowasAffected = await connection.ExecuteAsync(script);
-    }
-
-    public async Task Drop()
-    {
-        await ExecuteWithoutUsing($"ALTER DATABASE [{_databaseName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE");
-        await ExecuteWithoutUsing($"DROP DATABASE [{_databaseName}]");
     }
 }
